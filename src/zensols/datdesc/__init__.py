b3321303f800da750a40857fb741562bb08dd965
Paul Landes <landes@mailc.net>
2026-02-27 11:18:27 -0600

    doc

--- src/zensols/datdesc/__init__.py
+++ src/zensols/datdesc/__init__.py
@@ -2,20 +2,7 @@
 files to create tables from and their metadata is given as a YAML configuration
 file.
 
-Example::
-    latextablenamehere:
-        type: slack
-        slack_col: 0
-        path: ../config/table-name.csv
-        caption: Some Caption
-        placement: t!
-        size: small
-        single_column: true
-        percent_column_names: ['Proportion']
-
-
 """
-
 from zensols.util import APIError
 
 
